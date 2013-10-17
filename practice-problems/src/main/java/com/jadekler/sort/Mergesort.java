721e61814ebef9dff33071de3bdfccff7edb915c
Jean <jadekler@gmail.com>
2013-10-16 20:39:51 -0400

    Added completed version of mergesort

--- practice-problems/src/main/java/com/jadekler/sort/Mergesort.java
+++ practice-problems/src/main/java/com/jadekler/sort/Mergesort.java
@@ -9,13 +9,35 @@ import java.util.Random;
  */
 public class Mergesort 
 {
-    Random generator;
+    public int[] sort(int[] numbers) {
+        if (numbers.length == 1) {
+            return numbers;
+        } else {
+            int pivotPoint = (int)Math.ceil(numbers.length/2);
+            int[] lower = Arrays.copyOfRange(numbers, 0, pivotPoint);
+            int[] upper = Arrays.copyOfRange(numbers, pivotPoint, numbers.length);
 
-    public Mergesort() {
-        this.generator = new Random();
+            return merge(sort(lower), sort(upper));
+        }
     }
 
-    public int[] sort(int[] numbers) {
-        return numbers;
+    public int[] merge(int[] a, int[] b) {
+        int[] newArr = new int[a.length+b.length];
+        int a_i = 0;
+        int b_i = 0;
+        int na_i = 0;
+
+        while (na_i < newArr.length) {
+            if (b_i >= b.length || (a_i < a.length && a[a_i] < b[b_i])) {
+                newArr[na_i] = a[a_i];
+                a_i++;
+            } else {
+                newArr[na_i] = b[b_i];
+                b_i++;
+            }
+            na_i++;
+        }
+
+        return newArr;
     }
 }
