acc77e18218e1cae6daf4fb586f9861cf5456261
Jean <jadekler@gmail.com>
2013-10-26 18:46:58 -0400

    Added hashtable, will implement tomorrow as a break from red black trees

---
+++ practice-problems/src/main/java/com/jadekler/datastructures/HashMap.java
@@ -0,0 +1,15 @@
+package com.jadekler.datastructures;
+
+import java.util.Stack;
+
+/**
+ * This class holds items in a hash map
+ * Although I will implement slightly differently, I learned a lot of good ideas from a good read 
+ * that explains the principle here: http://www.techzoo.org/tutorials/how-to-create-your-own-hashmap.html
+ */
+public class HashMap
+{
+    public static void main(String args[]) {
+               
+    }
+}
