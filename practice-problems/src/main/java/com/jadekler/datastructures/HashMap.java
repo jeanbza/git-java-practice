b8823cddd7a46bd635d8854aaf1bfa828dd80f76
Jean <jadekler@gmail.com>
2013-10-27 17:05:38 -0400

    Skeleton for hashmap

--- practice-problems/src/main/java/com/jadekler/datastructures/HashMap.java
+++ practice-problems/src/main/java/com/jadekler/datastructures/HashMap.java
@@ -7,9 +7,38 @@ import java.util.Stack;
  * Although I will implement slightly differently, I learned a lot of good ideas from a good read 
  * that explains the principle here: http://www.techzoo.org/tutorials/how-to-create-your-own-hashmap.html
  */
-public class HashMap
+public class HashMap<K, V>
 {
+    private Node<K, V>[] buckets;
+
     public static void main(String args[]) {
                
+    }
+
+    public class Node<K, V> {
+        private Node<K, V> next;
+        private final K key;
+        private V value;
+
+        public Node(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        public V getValue() {
+            return this.value;
+        }
+    }
+
+    public HashMap() {
+
+    }
+
+    public V get(K key) {
+        return null;
+    }
+
+    public void put(K key, V value) {
+        
     }
 }
