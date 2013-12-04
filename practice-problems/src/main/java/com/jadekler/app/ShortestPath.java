2d4418ee7c5f0ad306637ea6e9eb393fb98ebc71
jadekler <jadekler@gmail.com>
2013-12-04 15:30:10 -0500

    Added vertices and edges using example graph (not code, just the edges+vertices in an example picture) at http://en.wikipedia.org/wiki/Dijkstra's_algorithm

--- practice-problems/src/main/java/com/jadekler/app/ShortestPath.java
+++ practice-problems/src/main/java/com/jadekler/app/ShortestPath.java
@@ -2,6 +2,16 @@ package com.jadekler.app;
 
 public class ShortestPath {
 	public static void main(String args[]) {
-		ShortestPath sp = new ShortestPath();
+		int[][] vertices = {{1,2,5},{0,2,3},{0,1,3,5},{1,2,4},{3,5},{0,4}};
+		int[][] edges = {{7,9,14},{7,10,15},{9,10,11,2},{15,11,6},{6,9},{14,9}};
+
+		ShortestPath sp = new ShortestPath(vertices, edges);
+		sp.djikstrasAlgorithm();
 	}
+
+	public ShortestPath(int[][] vertices, int[][] edges) {
+		
+	}
+
+	public void djikstrasAlgorithm() {}
 }
