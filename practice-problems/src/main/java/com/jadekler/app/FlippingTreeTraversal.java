0afb8ee6efbe54bc6e6a200c5d6c6852ab70a514
Jean <jadekler@gmail.com>
2013-12-18 16:14:45 -0500

    Added flipping tree problem with hello world template

---
+++ practice-problems/src/main/java/com/jadekler/app/FlippingTreeTraversal.java
@@ -0,0 +1,15 @@
+package com.jadekler.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main( String[] args ) {
+        System.out.println( "Hello World!" );
+    }
+
+    public boolean test() {
+        return true;
+    }
+}
