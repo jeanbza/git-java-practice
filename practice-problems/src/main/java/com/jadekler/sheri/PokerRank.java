6a9249b75d3dcc3c2c04a1e7b0612da8f264d13c
jadekler <jadekler@gmail.com>
2014-04-26 16:03:47 -0400

    Added a WONTADD to the poker one. Nope, nope, nope. I've tried poker / card problems before and for whatever reason I just suck at trying to figure out the rules (context: i SUCK at learning card games).

--- practice-problems/src/main/java/com/jadekler/sheri/PokerRank.java
+++ practice-problems/src/main/java/com/jadekler/sheri/PokerRank.java
@@ -7,6 +7,8 @@ package com.jadekler.sheri;
  * Hand: Ah As 10c 7d 6s (Pair of Aces)
  * Hand: Kh Kc 3s 3h 2d (2 Pair)
  * Hand: Kh Qh 6h 2h 9h (Flush)
+ *
+ * WONTADD: I am aggressively awful at poker
  */
 public class PokerRank {
     public static void main( String[] args ) {
