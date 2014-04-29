fe2cdfc205b958301a7eba4ae79c87a3d8fbdd23
jadekler <jadekler@gmail.com>
2014-04-29 17:25:24 -0400

    Added spiral numbers implemented using always-perfect-cubes

--- practice-problems/src/main/java/com/jadekler/sheri/SpiralNumbers.java
+++ practice-problems/src/main/java/com/jadekler/sheri/SpiralNumbers.java
@@ -10,11 +10,84 @@ package com.jadekler.sheri;
  * 16 15 14 13 12
  */
 public class SpiralNumbers {
-    public static void main( String[] args ) {
-        System.out.println( "Hello World!" );
+    public int[][] getSpiral(int num) {
+        int closestSquare = getClosestOddSquare(num);
+        int grid[][] = new int[closestSquare][closestSquare];
+        int center = (int) Math.ceil(closestSquare / 2);
+        int direction = 0;
+        int x = center;     // left/right
+        int y = center;     // up/down
+
+        grid[center][center] = -1;
+
+        for (int i = 1; i <= num; i++) {
+            if (direction % 4 == 0) {
+                // If you can go right, go right.
+                // Otherwise go up
+                if (grid[y][x+1] == 0) {
+                    direction++;
+                    x++;
+                } else {
+                    y--;
+                }
+            } else if (direction % 4 == 1) {
+                // If you can go down, go down.
+                // Otherwise go right
+                if (grid[y+1][x] == 0) {
+                    direction++;
+                    y++;
+                } else {
+                    x++;
+                }
+            } else if (direction % 4 == 2) {
+                // If you can go left, go left.
+                // Otherwise go down
+                if (grid[y][x-1] == 0) {
+                    direction++;
+                    x--;
+                } else {
+                    y++;
+                }
+            } else if (direction % 4 == 3) {
+                // If you can go up, go up.
+                // Otherwise go left
+                if (grid[y-1][x] == 0) {
+                    direction++;
+                    y--;
+                } else {
+                    x--;
+                }
+            }
+
+            grid[y][x] = i;
+        }
+
+        grid[center][center] = 0;
+
+        return grid;
     }
 
-    public boolean test() {
-        return true;
+    public void emitGrid(int[][] grid) {
+        String line;
+
+        for (int i = 0; i < grid.length; i++) {
+            line = "";
+
+            for (int j = 0; j < grid[i].length; j++) {
+                line += String.format("% 3d", grid[i][j])+" ";
+            }
+
+            System.out.println(line);
+        }
+    }
+
+    public int getClosestOddSquare(int num) {
+        int closestSquare = (int) Math.ceil(Math.sqrt(num));
+
+        if (closestSquare % 2 == 0) {
+            closestSquare ++;
+        }
+
+        return closestSquare;
     }
 }
--- practice-problems/src/test/java/com/jadekler/sheri/SpiralNumbersTest.java
+++ practice-problems/src/test/java/com/jadekler/sheri/SpiralNumbersTest.java
@@ -19,8 +19,38 @@ public class SpiralNumbersTest extends TestCase {
         return new TestSuite(SpiralNumbersTest.class);
     }
 
-    public void testTest() {
+    public void testGetClosestOddSquare() {
         SpiralNumbers target = new SpiralNumbers();
-        assertTrue(target.test());
+        assertEquals(target.getClosestOddSquare(5), 3);
+        assertEquals(target.getClosestOddSquare(10), 5);
+    }
+
+    public void testGetSpiralSimple() {
+        SpiralNumbers target = new SpiralNumbers();
+
+        int[][] expected = {{0,0,0},{5,0,1},{4,3,2}};
+        assertTrue(deepEquals(target.getSpiral(5), expected));
+    }
+
+    public boolean deepEquals(int[][] arr1, int[][] arr2) {
+        if (arr1.length != arr2.length) {
+            return false;
+        }
+
+        for (int i = 0; i < arr1.length; i++) {
+            if (arr1[i].length != arr2[i].length) {
+                return false;
+            }
+
+            for (int j = 0; j < arr1[i].length; j++) {
+                if (arr1[i][j] != arr2[i][j]) {
+
+                    System.out.println(arr1[i][j]+" "+arr2[i][j]);
+                    return false;
+                }
+            }
+        }
+
+        return true;
     }
 }
