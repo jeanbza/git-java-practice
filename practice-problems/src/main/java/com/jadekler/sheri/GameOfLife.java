10e8eef92c1c117018c8051dc23544ce8dfc6f99
jadekler <jadekler@gmail.com>
2014-04-29 22:39:15 -0400

    Intermediary commit for game of life, working on testing the numNeighbours function (after going a bit too far and having to work back after I found some bugs)

--- practice-problems/src/main/java/com/jadekler/sheri/GameOfLife.java
+++ practice-problems/src/main/java/com/jadekler/sheri/GameOfLife.java
@@ -1,8 +1,9 @@
 package com.jadekler.sheri;
 
 /**
- * Write some code that evolves generations through the "game of life". The input will be a game board of cells, either alive (1) or dead
- * (0). The code should take this board and create a new board for the next generation based on the following rules:
+ * Write some code that evolves generations through the "game of life". The input will be a game board of
+ * cells, either alive (1) or dead (0). The code should take this board and create a new board for the 
+ * next generation based on the following rules:
  * 1) Any live cell with fewer than two live neighbours dies (underpopulation)
  * 2) Any live cell with two or three live neighbours lives on to the next generation (survival)
  * 3) Any live cell with more than three live neighbours dies (overcrowding)
@@ -25,7 +26,56 @@ public class GameOfLife {
         System.out.println( "Hello World!" );
     }
 
-    public boolean test() {
-        return true;
+    public int[][] nextGeneration(int[][] board) {
+        int[][] newBoard = new int[board.length][board[0].length];
+
+        for (int y = 0; y < board.length; y++) {
+            for (int x = 0; x < board[y].length; x++) {
+                if (board[y][x] == 0 && numNeighbours(board, x, y) == 3) {
+                    newBoard[y][x] = 1;
+                } else if (numNeighbours(board, x, y) == 2 || numNeighbours(board, x, y) == 3) {
+                    newBoard[y][x] = 1;
+                }
+            }
+        }
+
+        return newBoard;
+    }
+
+    public int numNeighbours(int[][] grid, int x, int y) {
+        int numNeighbours = 0;
+        int tempX;
+        int tempY;
+
+        for (int i = -1; i <= 1; i++) {
+            for (int j = -1; j <= 1; j++) {
+                tempX = x+i;
+                tempY = y+j;
+
+                if (tempX >= 0 && tempX < grid[0].length && tempY >= 0 && tempY < grid.length && !(tempX == x && tempY == y)) {
+                    numNeighbours += grid[tempY][tempX];
+                }
+            }
+        }
+
+        System.out.println();
+
+        return numNeighbours;
+    }
+
+    public int[][] generateRandomBoard(int length) {
+        return generateRandomBoard(length, length);
+    }
+
+    public int[][] generateRandomBoard(int width, int height) {
+        int[][] board = new int[height][width];
+
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                board[i][j] = (int)(Math.random()*10)%2;
+            }
+        }
+
+        return board;
     }
 }
--- practice-problems/src/test/java/com/jadekler/sheri/GameOfLifeTest.java
+++ practice-problems/src/test/java/com/jadekler/sheri/GameOfLifeTest.java
@@ -19,8 +19,77 @@ public class GameOfLifeTest extends TestCase {
         return new TestSuite(GameOfLifeTest.class);
     }
 
-    public void testTest() {
-        GameOfLife target = new GameOfLife();
-        assertTrue(target.test());
+    public void emitGrid(int[][] grid) {
+        String line;
+
+        for (int i = 0; i < grid.length; i++) {
+            line = "";
+
+            for (int j = 0; j < grid[i].length; j++) {
+                line += String.format("% 3d", grid[i][j])+" ";
+            }
+
+            System.out.println(line);
+        }
     }
+
+    public boolean deepEquals(int[][] arr1, int[][] arr2) {
+        if (arr1.length != arr2.length) {
+            return false;
+        }
+
+        for (int i = 0; i < arr1.length; i++) {
+            if (arr1[i].length != arr2[i].length) {
+                return false;
+            }
+
+            for (int j = 0; j < arr1[i].length; j++) {
+                if (arr1[i][j] != arr2[i][j]) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    public void testNumNeightboursZero() {
+        GameOfLife target = new GameOfLife();
+        int expected = 0;
+        int[][] grid = {{1,0},{0,0}};
+        int x = 0;
+        int y = 0;
+
+        assertEquals(target.numNeighbours(grid, x, y), expected);
+    }
+
+    public void testNumNeightboursSimple() {
+        GameOfLife target = new GameOfLife();
+        int expected = 1;
+        int[][] grid = {{1,0},{0,1}};
+        int x = 0;
+        int y = 0;
+
+        assertEquals(expected, target.numNeighbours(grid, x, y));
+
+        expected = 3;
+        int[][] newgrid = {{0,1},{1,1}};
+        x = 0;
+        y = 0;
+
+        assertEquals(expected, target.numNeighbours(newgrid, x, y));
+    }
+
+    // public void testStandard() {
+    //     GameOfLife target = new GameOfLife();
+
+    //     int[][] board = {{0, 1, 0, 0, 0},{1, 0, 0, 1, 1},{1, 1, 0, 0, 1},{0, 1, 0, 0, 0},{1, 0, 0, 0, 1}};
+    //     int[][] expected = {{0, 0, 0, 0, 0},{1, 0, 1, 1, 1},{1, 1, 1, 1, 1},{0, 1, 0, 0, 0},{0, 0, 0, 0, 0}};
+
+    //     emitGrid(board);
+    //     System.out.println();
+    //     emitGrid(target.nextGeneration(board));
+
+    //     assertTrue(deepEquals(target.nextGeneration(board), expected));
+    // }
 }
