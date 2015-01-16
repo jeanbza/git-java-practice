8684e1562b7a26d7d006aca136dedc86db211ed8
Jean de Klerk <jadekler@gmail.com>
2015-01-16 15:47:26 -0700

    wip

--- spring-tutorials/ping-pong-pi/src/main/java/hello/GreetingController.java
+++ spring-tutorials/ping-pong-pi/src/main/java/hello/GreetingController.java
@@ -1,9 +1,12 @@
 package hello;
 
+import javax.sql.DataSource;
+import org.springframework.jdbc.core.*;
+import org.springframework.web.bind.annotation.*;
+
+import java.sql.*;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GreetingController {
@@ -12,8 +15,26 @@ public class GreetingController {
     private final AtomicLong counter = new AtomicLong();
 
     @RequestMapping("/greeting")
-    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
-        return new Greeting(counter.incrementAndGet(),
-                            String.format(template, name));
+    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) throws SQLException {
+//        MysqlDataSource mysqlDS = new MysqlDataSource();
+//        mysqlDS.setURL(props.getProperty("MYSQL_DB_URL"));
+//        mysqlDS.setUser(props.getProperty("MYSQL_DB_USERNAME"));
+//        mysqlDS.setPassword(props.getProperty("MYSQL_DB_PASSWORD"));
+//
+//        JdbcTemplate jdbcTemplate = new JdbcTemplate(mysql);
+//
+//        List<String> results = jdbcTemplate.query(
+//            "select boom from test.bam where first_name = ?", new Object[] { "Josh" },
+//            new RowMapper<String>() {
+//                @Override
+//                public String mapRow(ResultSet rs, int rowNum) throws SQLException {
+//                    return rs.getString("boom");
+//                }
+//            }
+//        );
+//
+//        System.out.println(results);
+
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
     }
 }
