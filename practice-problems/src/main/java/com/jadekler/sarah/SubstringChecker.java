6f26de095cf368903e30f22b9eb08c530a05a801
Jean de Klerk <jadekler@gmail.com>
2015-03-04 20:43:11 -0700

    Working substring checker. o(n-m), which is not great. Looking to make it better.

--- practice-problems/src/main/java/com/jadekler/sarah/SubstringChecker.java
+++ practice-problems/src/main/java/com/jadekler/sarah/SubstringChecker.java
@@ -2,6 +2,12 @@ package com.jadekler.sarah;
 
 public class SubstringChecker {
     public boolean isSubstring(String source, String target) {
+        for (int i = 0; i < source.length() - target.length(); i++) {
+            if (source.substring(i, i + target.length()).equals(target)) {
+                return true;
+            }
+        }
+
         return false;
     }
 }
